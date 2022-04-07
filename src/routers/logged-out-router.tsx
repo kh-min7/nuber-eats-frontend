bc787d09f4ada2f796ee5892149df75513df957f
MIN <rudgh2879@naver.com>
2022-04-07 23:52:14 +0900

    15.2 React Hook Form part Two

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,6 +1,10 @@
 import React from "react";
-import { isLoggedInVar } from "../apollo";
 import { useForm } from "react-hook-form";
+
+interface IForm {
+  email: string;
+  password: string;
+}
 
 export const LoggedOutRouter = () => {
   const {
@@ -8,14 +12,14 @@ export const LoggedOutRouter = () => {
     watch,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<IForm>();
   const onSubmit = () => {
     console.log(watch());
   };
   const onInvalid = () => {
     console.log("cant create account");
   };
-  console.log(errors);
+
   return (
     <div>
       <h1>Logged Out</h1>
@@ -30,6 +34,14 @@ export const LoggedOutRouter = () => {
             type="email"
             placeholder="email"
           />
+          {errors.email?.message && (
+            <span className="font-bold text-red-600">
+              {errors.email?.message}
+            </span>
+          )}
+          {errors.email?.type === "pattern" && (
+            <span className="font-bold text-red-600">Only gmail allowed</span>
+          )}
         </div>
         <div>
           <input
