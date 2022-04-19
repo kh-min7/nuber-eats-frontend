7dd5873801fe3cfa0af31e3520389fd0fdadbd8d
MIN <rudgh2879@naver.com>
2022-04-19 22:48:08 +0900

    17.8 Restaurant part One

---
+++ src/pages/client/restaurant.tsx
@@ -0,0 +1,40 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { RESTAURANT_FRAGMENT } from "../../fragments";
+import {
+  restaurant,
+  restaurantVariables,
+} from "../../__generated__/restaurant";
+
+const RESTAURANT_QUERY = gql`
+  query restaurant($input: RestaurantInput!) {
+    restaurant(input: $input) {
+      ok
+      error
+      restaurant {
+        ...RestaurantParts
+      }
+    }
+  }
+  ${RESTAURANT_FRAGMENT}
+`;
+
+interface IRestaurantParams {
+  id: string;
+}
+
+export const Restaurant = () => {
+  const { id } = useParams() as IRestaurantParams;
+  const { loading, data } = useQuery<restaurant, restaurantVariables>(
+    RESTAURANT_QUERY,
+    {
+      variables: {
+        input: {
+          restaurantId: +id,
+        },
+      },
+    }
+  );
+  return <h1>Restaurant{id}</h1>;
+};
