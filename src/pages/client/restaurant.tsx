20c2bcc232e401ab6dbfb3c60e4c1304e9146f01
MIN <rudgh2879@naver.com>
2022-04-19 23:05:40 +0900

    17.9 Restaurant part Two

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
 import { RESTAURANT_FRAGMENT } from "../../fragments";
 import {
@@ -36,5 +37,28 @@ export const Restaurant = () => {
       },
     }
   );
-  return <h1>Restaurant{id}</h1>;
+  return (
+    <div>
+      <Helmet>
+        <title>{data?.restaurant.restaurant?.name || ""} | Nuber Eats</title>
+      </Helmet>
+      <div
+        className=" bg-gray-800 bg-center bg-cover py-48"
+        style={{
+          backgroundImage: `url(${data?.restaurant.restaurant?.coverImg})`,
+        }}
+      >
+        <div className="bg-white w-3/12 py-8 pl-48">
+          <h4 className="text-4xl mb-3">{data?.restaurant.restaurant?.name}</h4>
+          <h5 className="text-sm font-light mb-2">
+            {data?.restaurant.restaurant?.category?.name}
+          </h5>
+          <h6 className="text-sm font-light">
+            {data?.restaurant.restaurant?.address}
+          </h6>
+        </div>
+      </div>
+    </div>
+    // 카테고리들 링크 달기(코드챌린지)
+  );
 };
