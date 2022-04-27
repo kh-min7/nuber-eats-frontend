1075f36a1a393af0b47a84e0dcf7985d7eee7fe7
MIN <rudgh2879@naver.com>
2022-04-27 20:30:45 +0900

    19.7 EditProfile E2E part Two

--- cypress/integration/user/edit-profile.ts
+++ cypress/integration/user/edit-profile.ts
@@ -9,6 +9,12 @@ describe("Edit Profile", () => {
     user.title().should("eq", "Edit Profile | Nuber Eats");
   });
   it("can change email", () => {
+    user.intercept("POST", "http://localhost:4000/graphql", (req) => {
+      if (req.body?.operationName === "editProfile") {
+        // @ts-ignore
+        req.body?.variables?.input?.email = "rudgh2879@naver.com";
+      }
+    });
     user.visit("/edit-profile");
     user.findByPlaceholderText(/email/i).clear().type("new@naver.com");
     user.findByRole("button").click();
