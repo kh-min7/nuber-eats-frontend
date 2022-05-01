9e583b937e160a5a6d68328f4a5f36b4dc98f6fa
MIN <rudgh2879@naver.com>
2022-05-01 13:33:47 +0900

    20.5 Cache Optimization part One

--- src/pages/owner/add-restaurants.tsx
+++ src/pages/owner/add-restaurants.tsx
@@ -14,6 +14,7 @@ const CREATE_RESTAURANT_MUTATION = gql`
     createRestaurant(input: $input) {
       error
       ok
+      restaurantId
     }
   }
 `;
@@ -28,7 +29,7 @@ interface IFormProps {
 export const AddRestaurant = () => {
   const onCompleted = (data: createRestaurant) => {
     const {
-      createRestaurant: { ok, error },
+      createRestaurant: { ok, restaurantId },
     } = data;
     if (ok) {
       setUploading(false);
